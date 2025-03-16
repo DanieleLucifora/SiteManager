e8f26915de7ca017d79f09067a62ba938446af7c
Fernando Riccioli <ricciolifernando@gmail.com>
2025-03-16 10:08:12 +0100

    Miglioramento Login e UI Task

--- SiteManager/LoginPage.xaml.cs
+++ SiteManager/LoginPage.xaml.cs
@@ -2,23 +2,25 @@ namespace SiteManager;
 
 public partial class LoginPage : ContentPage
 {
-	public LoginPage()
-	{
-		InitializeComponent();
-	}
+    public LoginPage()
+    {
+        InitializeComponent();
+        UsernameEntry.Completed += (s, e) => PasswordEntry.Focus();
+        PasswordEntry.Completed += (s, e) => LoginButton_Clicked(s, e);
+    }
 
     private async void LoginButton_Clicked(object sender, EventArgs e)
     {
-		string username = UsernameEntry.Text;
-		string password = PasswordEntry.Text;
+        string username = UsernameEntry.Text;
+        string password = PasswordEntry.Text;
 
-		if (username == "admin" && password == "password")
-		{
+        if (username == "admin" && password == "password")
+        {
             await Navigation.PushAsync(new MainPage());
-		}
-		else
-		{
-			await DisplayAlert("Errore", "Username o password errati", "OK");
-		}
-    }	
+        }
+        else
+        {
+            await DisplayAlert("Errore", "Username o password errati", "OK");
+        }
+    }
 }
