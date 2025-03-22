d95ad156c837cf02c23b6f44e856391f1257296a
Fernando Riccioli <ricciolifernando@gmail.com>
2025-03-22 10:52:53 +0100

    Semplificazione TasksPage.xaml.cs

--- SiteManager/TasksPage.xaml.cs
+++ SiteManager/TasksPage.xaml.cs
@@ -7,41 +7,28 @@ namespace SiteManager;
 public partial class TasksPage : ContentPage
 {
 	public ObservableCollection<Tasks> TasksList{ get; set; }	
-   	private Cantiere _selectedCantiere;
+   	private Cantiere cantiere;
 
 	public TasksPage(Cantiere selectedCantiere)
 	{
 		InitializeComponent();
-        TasksList = new ObservableCollection<Tasks>();
-        _selectedCantiere = selectedCantiere;
-        BindingContext = this;		
+        TasksList = [];
+        cantiere = selectedCantiere;	
         LoadTasks();		
 	}
 
     private void LoadTasks()
     {
-        var tasks = TasksService.OttieniTasks(_selectedCantiere);
+        var tasks = TasksService.OttieniTasks(cantiere);
+
         foreach (var task in tasks)
         {
-            bool taskEsistente = false;
-            foreach (var iTask in TasksList)
-            {
-                if (iTask.IdTasks == task.IdTasks)
-                {
-                    taskEsistente = true;
-                    break;
-                }
-            }
-
-            if (!taskEsistente)
-            {
-                TasksList.Add(task);
-            }
+            TasksList.Add(task);
         }
         TasksCollectionView.ItemsSource = TasksList;
     }
     
-    private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    private void OnSelectionChanged(object sender, SelectionChangedEventArgs e) //
     {
         if (e.CurrentSelection.FirstOrDefault() is Tasks selectedTask)
         {
@@ -52,23 +39,20 @@ public partial class TasksPage : ContentPage
     }
     private void VisualizzaTask_Clicked(object sender, EventArgs e)
     {
-        var button = sender as Button;
-        var task = button?.BindingContext as Tasks;
+        Button button = (Button)sender;
+        Tasks task = (Tasks)button.BindingContext;
         if (task != null)
         {
-            // Logica per visualizzare i dettagli del task
             DisplayAlert("Dettagli Task", $"Descrizione: {task.Descrizione}\nData: {task.Data.ToShortDateString()}", "OK");
         }
     }
 
-	private async void AggiungiTask_Clicked(object sender, EventArgs e)
+	private void AggiungiTask_Clicked(object sender, EventArgs e)
 	{
 		FormStackLayout.IsVisible = true;
         SalvaTaskBtn.IsVisible = true;
         NuovoTaskBtn.IsVisible = false;
-        AggiornaTaskBtn.IsVisible = false;
         ClearForm();
-        await Task.CompletedTask;
 	}
     private async void SalvaTask_Clicked(object sender, EventArgs e)
     {
@@ -80,13 +64,12 @@ public partial class TasksPage : ContentPage
         
         string descrizione = DescrizioneEntry.Text;
         DateTime data = DataPicker.Date;
-        int cantiereId = _selectedCantiere.IdCantiere;
 
         Tasks nuovaTask = new Tasks
         {
             Descrizione = descrizione,
             Data = data,
-            CantiereId = cantiereId
+            CantiereId = cantiere.IdCantiere
         };
 
         bool success = TasksService.AggiungiTask(nuovaTask);
@@ -97,7 +80,7 @@ public partial class TasksPage : ContentPage
             NuovoTaskBtn.IsVisible = true;
             FormStackLayout.IsVisible = false;
             SalvaTaskBtn.IsVisible = false;
-            LoadTasks();
+            TasksList.Add(nuovaTask);
             ClearForm();
         }
         else
@@ -106,77 +89,70 @@ public partial class TasksPage : ContentPage
         }
     }
 
-    private async void ModificaTask_Clicked(object sender, EventArgs e)
+    private void ModificaTask_Clicked(object sender, EventArgs e)
 	{
-        if (sender is Button button && button.CommandParameter is Tasks selectedTask)
-        {
-            // Popola i campi del form con i dati del task selezionato
-            DescrizioneEntry.Text = selectedTask.Descrizione;
-            DataPicker.Date = selectedTask.Data;
+        Button button = (Button)sender;
+        Tasks task = (Tasks)button.CommandParameter;
+        
+        DescrizioneEntry.Text = task.Descrizione;
+        DataPicker.Date = task.Data;
 
-            // Imposta il BindingContext del pulsante AggiornaOperaioBtn
-            AggiornaTaskBtn.BindingContext = selectedTask;
+        AggiornaTaskBtn.BindingContext = task;
 
-            // Rendi visibile il form e il pulsante di aggiornamento
-            FormStackLayout.IsVisible = true;
-            AggiornaTaskBtn.IsVisible = true;
-            NuovoTaskBtn.IsVisible = false;
-            SalvaTaskBtn.IsVisible = false; // Nascondi il pulsante di salvataggio se necessario
-
-            await Task.CompletedTask;
-        }
-        else
-        {
-            await DisplayAlert("Errore", "Nessun task selezionato", "OK");
-        }
+        FormStackLayout.IsVisible = true;
+        AggiornaTaskBtn.IsVisible = true;
+        NuovoTaskBtn.IsVisible = false;
+        SalvaTaskBtn.IsVisible = false;
 	}
 
     private async void AggiornaTask_Clicked(object sender, EventArgs e)
     {
-        if (AggiornaTaskBtn.BindingContext is Tasks selectedTask)
-        {
-            // Aggiorna i dati del cantiere con i valori del form
-            selectedTask.Descrizione = DescrizioneEntry.Text;
-            selectedTask.Data = DataPicker.Date;
-         
-            DisplayAlert("Dettagli Task", $"Id Task: {selectedTask.IdTasks}\nDescrizione: {selectedTask.Descrizione} \nData: {selectedTask.Data.ToShortDateString()}", "OK");
+        Tasks task = (Tasks)AggiornaTaskBtn.BindingContext;
 
-            bool success = TasksService.AggiornaTask(selectedTask);
+        task.Descrizione = DescrizioneEntry.Text;
+        task.Data = DataPicker.Date;
+         
+        await DisplayAlert("Dettagli Task", $"Id Task: {task.IdTasks}\nDescrizione: {task.Descrizione} \nData: {task.Data.ToShortDateString()}", "OK");
+
+        bool success = TasksService.AggiornaTask(task);
+
+        if (success)
+        {
+            await DisplayAlert("Successo", "Task aggiornato con successo", "OK");
+            TasksCollectionView.ItemsSource = null;
+            TasksCollectionView.ItemsSource = TasksList;
+        }
+        else
+        {
+            await DisplayAlert("Errore", "Si è verificato un errore durante l'aggiornamento del task", "OK");
+        }
+
+        FormStackLayout.IsVisible = false;
+        AggiornaTaskBtn.IsVisible = false;
+        NuovoTaskBtn.IsVisible = true;
+        ClearForm();
+    }
+
+    private async void EliminaTask_Clicked(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        Tasks task = (Tasks)button.CommandParameter;
+
+        bool conferma = await DisplayAlert("Conferma Eliminazione", $"Sei sicuro di voler cancellare il task?", "Si", "No");
+
+        if (conferma)
+        {
+            bool success = TasksService.EliminaTask(task.IdTasks);
+
             if (success)
             {
-                TasksCollectionView.ItemsSource = null;
-                TasksCollectionView.ItemsSource = TasksList;
-                await DisplayAlert("Successo", "Task aggiornato con successo", "OK");
+                TasksList.Remove(task);
+                await DisplayAlert("Successo", "Task cancellato con successo", "OK");                
             }
             else
             {
-                await DisplayAlert("Errore", "Si è verificato un errore durante l'aggiornamento del task", "OK");
-            }    
-            FormStackLayout.IsVisible = false;
-            AggiornaTaskBtn.IsVisible = false;
-            NuovoTaskBtn.IsVisible = true;
-            ClearForm();
-        }
-    }
-    private async void EliminaTask_Clicked(object sender, EventArgs e)
-    {
-        if (sender is Button button && button.CommandParameter is Tasks selectedTask)
-        {
-            bool conferma = await DisplayAlert("Conferma Eliminazione", $"Sei sicuro di voler cancellare il task?", "Si", "No");
-            if (conferma)
-            {
-                bool success = TasksService.EliminaTask(selectedTask.IdTasks);
-                if (success)
-                {
-                    TasksList.Remove(selectedTask);
-                    await DisplayAlert("Successo", "Task cancellato con successo", "OK"); 
-                    ClearForm();                
-                }
-                else
-                {
-                    await DisplayAlert("Errore", "Si è verificato un errore durante la cancellazione del task", "OK");
-                }                  
-            }             
+                await DisplayAlert("Errore", "Si è verificato un errore durante la cancellazione del task", "OK");
+            }                  
         }
     } 
 
